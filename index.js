111c29e10027d25362aecb4a94f54dfd54b7c525
SnowKitty <jcaibmi2@gmail.com>
2020-11-25 05:16:25 +0000

    Added packages & SQL db

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express'); 
+const sha1 = require("sha1");
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get("/signup/:email", (req, res) => {
+  //receive email, form a link, send email with the link
+  let email = req.params.email;
+  console.log(email);
+  
+  res.json({"success": email});
+})
+
+const cors = require('cors');
+
+// set up port
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+
+// add routes
+const router = require('./routes/router.js');
+app.use('/api', router);
+
+// run server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
