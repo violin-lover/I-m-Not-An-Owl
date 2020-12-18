67cd5807e0e1915a56073ca27e9061b15b643784
SnowKitty <jcaibmi2@gmail.com>
2020-12-18 04:13:17 +0000

    messages go to db

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express'); 
+const express = require('express');
 const sha1 = require("sha1");
+const sendmail = require('./lib/sendEmail.js');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
-const sendMail = require('./lib/sendEmail.js')
-
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,27 +10,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-//will send the new user a link to the signup form
+//sends the user a link through email  that directs the user to the sign up form.
 
-app.get("/verify/:email", (req, res) => {
+app.get("/verifyemail/:email", (req, res) => {
   //receive email, form a link, send email with the link
   let email = req.params.email;
-    console.log("verifying" + " " + email);
-    sendMail(email)
-    res.json({"success" : email});
+  console.log(email);
+  sendmail(email);
+  res.json({ "success": email});
 })
 
-const cors = require('cors');
 
 // set up port
 const PORT = 3000;
 
-app.use(bodyParser.json())
 app.use(cors());
-
 // add routes
+
 const router = require('./routes/router.js');
 app.use('/api', router);
-
 // run server
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- routes/router.js
+++ routes/router.js
@@ -1,120 +1,136 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const uuid = require("uuid");
-const jwt = require("jsonwebtoken");
-const db = require("../lib/db.js");
-const userMiddleware = require("../middleware/users.js");
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+const jwt = require('jsonwebtoken');
+const db = require('../lib/db.js');
 
-router.post("/sign-up", userMiddleware.validateRegister, (req, res, next) => {
-	console.log(req.body);
-	console.log("trying to sign up");
-	console.log('req.body.email:'+ db.escape(req.body.email));
-	console.log('req.body.password:'+ db.escape(req.body.password));
-    db.query(
-			`SELECT * FROM Users WHERE LOWER(id) = LOWER(${db.escape(
-				req.body.email)});`, 
-				(err, result) => {
-        if (result.length > 0) {
-        	console.log('409')
-					console.log(result)
-            return res.status(409).send({
-                msg: "This email is already in use!",
-            });
-        } else {
-            // username is available
-						console.log('entering bcrypt')
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                if (err) {
-                	console.log(err)
-									console.log(db.escape(hash))
-                    return res.status(500).send({
-                        msg: err,
-                    });
-                } else {
-                    // has hashed pw => add to database
-                    db.query(`INSERT INTO Users (id, username, password, registered) VALUES ('${req.body.email}', ${db.escape(
-											req.body.username
-											)}, ${db.escape(hash)}, now())`,
-											 (err, result) => {
-                        if (err) {
-                            throw err;
-                            console.log(err)
-														console.log(hash)
-                            return res.status(400).send({
-                                msg: err,
-                            });
-                        }
-                        console.log("reached here")
-                        return res.status(201).send({
-                            msg: "Registered!",
-                        });
-                    });
-                }
-            });
-        }
-    });
-});
-
-router.post("/login", (req, res, next) => {
-    db.query(`SELECT * FROM Users WHERE username = ${db.escape(req.body.username)};`, (err, result) => {
-        // user does not exists
-        if (err) {
-            throw err;
-            return res.status(400).send({
-                msg: err,
-            });
-        }
-        if (!result.length) {
-            return res.status(401).send({
-                msg: "Username or password is incorrect!",
-            });
-        }
-        // check password
-        bcrypt.compare(req.body.password, result[0]["password"], (bErr, bResult) => {
-            // wrong password
-            if (bErr) {
-                throw bErr;
-                return res.status(401).send({
-                    msg: "Username or password is incorrect!",
-                });
-            }
-            if (bResult) {
-                const token = jwt.sign(
-                    {
-                        username: result[0].username,
-                        userId: result[0].id,
-                    },
-                    'SECRETKEY',
-                    {
-                        expiresIn: "7d",
-                    }
-                );
-                db.query(`UPDATE Users SET last_login = now() WHERE id = '${result[0].id}'`);
-                return res.status(200).send({
-                    msg: "Logged in!",
-                    token: token,
-                    user: result[0],
-                });
-            }
-            return res.status(401).send({
-                msg: "Username or password is incorrect!",
-            });
-        });
-    });
-});
-
+const userMiddleware = require('../middleware/users.js');
 
 router.get('/secret-route', userMiddleware.isLoggedIn, (req, res, next) => {
   console.log(req.userData);
-  res.send('Welcome ' + req.userData.username + '. This is the secret content. Only logged in users can see that!');
+  res.send('Welcome!' + req.userData.username + 'This is the secret content. Only logged in users can see that!');
+
 });
 
 router.post('/post-message', userMiddleware.isLoggedIn, (req, res, next) => {
   console.log(req.userData.username);
   console.log(req.body.message);
-  res.send('Welcome ' + req.userData.username + '. This is the secret content. Only logged in users can see that!');
+     db.query(
+              `INSERT INTO Messages (author, message, posted_at) VALUES ('${req.userData.username}', ${db.escape(
+                req.body.message
+              )}, now())`,
+              (err, result) => {
+                if (err) {
+                  throw err;
+                  return res.status(400).send({
+                    msg: err
+                  });
+                }
+                return res.status(201).send({
+                  msg: 'Sent!'
+                });
+              }
+            );
 });
 
+router.post('/sign-up', userMiddleware.validateRegister, (req, res, next) => {
+  db.query(
+    `SELECT * FROM Users WHERE LOWER(username) = LOWER(${db.escape(
+      req.body.username
+    )});`,
+    (err, result) => {
+      if (result.length) {
+        return res.status(409).send({
+          msg: 'This username is already in use!'
+        });
+      } else {
+        // username is available
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).send({
+              msg: err
+            });
+          } else {
+            // has hashed pw => add to database
+            db.query(
+              `INSERT INTO Users (id, username, password, registered) VALUES ('${req.body.email}', ${db.escape(
+                req.body.username
+              )}, ${db.escape(hash)}, now())`,
+              (err, result) => {
+                if (err) {
+                  throw err;
+                  return res.status(400).send({
+                    msg: err
+                  });
+                }
+                return res.status(201).send({
+                  msg: 'Registered!'
+                });
+              }
+            );
+          }
+        });
+      }
+    }
+  );
+});
+
+router.post('/login', (req, res, next) => {
+  console.log("entering/logging in with " + req.body.username);
+  console.log(req.body);
+  db.query(
+    `SELECT * FROM Users WHERE username = ${db.escape(req.body.username)};`,
+    (err, result) => {
+      // user does not exists
+      if (err) {
+        throw err;
+        return res.status(400).send({
+          msg: err
+        });
+      }
+      if (!result.length) {
+        return res.status(401).send({
+          msg: "You don't exist"
+        });
+      }
+      // check password
+      bcrypt.compare(
+        req.body.password,
+        result[0]['password'],
+        (bErr, bResult) => {
+          // wrong password
+          if (bErr) {
+            throw bErr;
+            return res.status(401).send({
+              msg: 'Password is incorrect!'
+            });
+          }
+          if (bResult) {
+            const token = jwt.sign({
+              username: result[0].username,
+              userId: result[0].id
+            },
+              'SECRETKEY', {
+                expiresIn: '7d'
+              }
+            );
+            db.query(
+              `UPDATE Users SET last_login = now() WHERE id = '${result[0].id}'`
+            );
+            return res.status(200).send({
+              msg: 'Logged in!',
+              token,
+              user: result[0]
+            });
+          }
+          return res.status(401).send({
+            msg: 'Username or password is incorrect!'
+          });
+        }
+      );
+    }
+  );
+});
 
 module.exports = router;
