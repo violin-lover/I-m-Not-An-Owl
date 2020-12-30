5d28d489e98c739327f70f7f4308bba2c0e991f4
SnowiiVA <jcaibmi2@gmail.com>
2020-12-30 04:01:55 +0000

    can successfully like

--- routes/router.js
+++ routes/router.js
@@ -1,150 +1,145 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const db = require('../lib/db.js');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const db = require("../lib/db.js");
+const userMiddleware = require("../middleware/users.js");
 
-const userMiddleware = require('../middleware/users.js');
-
-router.get('/secret-route', userMiddleware.isLoggedIn, (req, res, next) => {
-  console.log(req.userData);
-  res.send('Welcome!' + req.userData.username + 'This is the secret content. Only logged in users can see that!');
+router.get("/secret-route", userMiddleware.isLoggedIn, (req, res, next) => {
+    console.log(req.userData);
+    res.send("Welcome!" + req.userData.username + "This is the secret content. Only logged in users can see that!");
 });
 
-router.get('/read-message', userMiddleware.isLoggedIn, (req, res, next) => {
-  console.log(req.userData);
-          db.query(
-              `SELECT * FROM Messages`,
-              (err, result) => {
-                if (err) {
-                  throw err;
-                }
-                console.log(result)
-                 res.json(result);
-              }
-            );
-});
-
-router.post('/post-message', userMiddleware.isLoggedIn, (req, res, next) => {
-  console.log(req.userData.username);
-  console.log(req.body.message);
-     db.query(
-              `INSERT INTO Messages (author, message, posted_at) VALUES ('${req.userData.username}', ${db.escape(
-                req.body.message
-              )}, now())`,
-              (err, result) => {
-                if (err) {
-                  throw err;
-                  return res.status(400).send({
-                    msg: err
-                  });
-                }
-                return res.status(201).send({
-                  msg: 'Sent!'
-                });
-              }
-            );
-});
-
-router.post('/sign-up', userMiddleware.validateRegister, (req, res, next) => {
-  db.query(
-    `SELECT * FROM Users WHERE LOWER(username) = LOWER(${db.escape(
-      req.body.username
-    )});`,
-    (err, result) => {
-      if (result.length) {
-        return res.status(409).send({
-          msg: 'This username is already in use!'
-        });
-      } else {
-        // username is available
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).send({
-              msg: err
-            });
-          } else {
-            // has hashed pw => add to database
-            db.query(
-              `INSERT INTO Users (id, username, password, registered) VALUES ('${req.body.email}', ${db.escape(
-                req.body.username
-              )}, ${db.escape(hash)}, now())`,
-              (err, result) => {
-                if (err) {
-                  throw err;
-                  return res.status(400).send({
-                    msg: err
-                  });
-                }
-                return res.status(201).send({
-                  msg: 'Registered!'
-                });
-              }
-            );
-          }
-        });
-      }
-    }
-  );
-});
-
-router.post('/login', (req, res, next) => {
-  console.log("entering/logging in with " + req.body.username);
-  console.log(req.body);
-  db.query(
-    `SELECT * FROM Users WHERE username = ${db.escape(req.body.username)};`,
-    (err, result) => {
-      // user does not exists
-      if (err) {
-        throw err;
-        return res.status(400).send({
-          msg: err
-        });
-      }
-      if (!result.length) {
-        return res.status(401).send({
-          msg: "You don't exist"
-        });
-      }
-      // check password
-      bcrypt.compare(
-        req.body.password,
-        result[0]['password'],
-        (bErr, bResult) => {
-          // wrong password
-          if (bErr) {
-            throw bErr;
-            return res.status(401).send({
-              msg: 'Password is incorrect!'
-            });
-          }
-          if (bResult) {
-            const token = jwt.sign({
-              username: result[0].username,
-              userId: result[0].id
-            },
-              'SECRETKEY', {
-                expiresIn: '7d'
-              }
-            );
-            db.query(
-              `UPDATE Users SET last_login = now() WHERE id = '${result[0].id}'`
-            );
-            return res.status(200).send({
-              msg: 'Logged in!',
-              token,
-              user: result[0]
-            });
-          }
-          return res.status(401).send({
-            msg: 'Username or password is incorrect!'
-          });
+router.get("/read-message", userMiddleware.isLoggedIn, (req, res, next) => {
+    console.log(req.userData);
+    db.query(`SELECT * FROM Messages`, (err, result) => {
+        if (err) {
+            throw err;
         }
-      );
-    }
+        console.log(result);
+        res.json(result);
+    });
+});
+
+router.post("/post-message", userMiddleware.isLoggedIn, (req, res, next) => {
+    console.log(req.userData.username);
+    console.log(req.body.message);
+    db.query(`INSERT INTO Messages (author, message, posted_at) VALUES ('${req.userData.username}', ${db.escape(req.body.message)}, now())`, (err, result) => {
+        if (err) {
+            throw err;
+            return res.status(400).send({
+                msg: err,
+            });
+        }
+        return res.status(201).send({
+            msg: "Sent!",
+        });
+    });
+});
+
+router.post("/like-message", userMiddleware.isLoggedIn, (req, res, next) => {
+    console.log(req.userData.username);
+    console.log(req.body.id);
+    db.query(
+        `UPDATE Messages 
+            SET post_likes = post_likes + 1
+            WHERE id = ${req.body.id}`,(err, result) => {
+              if (err) {
+                throw err;
+                return res.status(400).send({
+                  msg: err,
+                })
+              }
+
+              return res.status(201).send({
+                msg: "Liked successfully"
+              })
+            }
   );
 });
 
-router.post("/likeMessage",)  
+router.post("/sign-up", userMiddleware.validateRegister, (req, res, next) => {
+    db.query(`SELECT * FROM Users WHERE LOWER(username) = LOWER(${db.escape(req.body.username)});`, (err, result) => {
+        if (result.length) {
+            return res.status(409).send({
+                msg: "This username is already in use!",
+            });
+        } else {
+            // username is available
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
+                    return res.status(500).send({
+                        msg: err,
+                    });
+                } else {
+                    // has hashed pw => add to database
+                    db.query(`INSERT INTO Users (id, username, password, registered) VALUES ('${req.body.email}', ${db.escape(req.body.username)}, ${db.escape(hash)}, now())`, (err, result) => {
+                        if (err) {
+                            throw err;
+                            return res.status(400).send({
+                                msg: err,
+                            });
+                        }
+                        return res.status(201).send({
+                            msg: "Registered!",
+                        });
+                    });
+                }
+            });
+        }
+    });
+});
+
+router.post("/login", (req, res, next) => {
+    console.log("entering/logging in with " + req.body.username);
+    console.log(req.body);
+    db.query(`SELECT * FROM Users WHERE username = ${db.escape(req.body.username)};`, (err, result) => {
+        // user does not exists
+        if (err) {
+            throw err;
+            return res.status(400).send({
+                msg: err,
+            });
+        }
+        if (!result.length) {
+            return res.status(401).send({
+                msg: "You don't exist",
+            });
+        }
+        // check password
+        bcrypt.compare(req.body.password, result[0]["password"], (bErr, bResult) => {
+            // wrong password
+            if (bErr) {
+                throw bErr;
+                return res.status(401).send({
+                    msg: "Password is incorrect!",
+                });
+            }
+            if (bResult) {
+                const token = jwt.sign(
+                    {
+                        username: result[0].username,
+                        userId: result[0].id,
+                    },
+                    "SECRETKEY",
+                    {
+                        expiresIn: "7d",
+                    }
+                );
+                db.query(`UPDATE Users SET last_login = now() WHERE id = '${result[0].id}'`);
+                return res.status(200).send({
+                    msg: "Logged in!",
+                    token,
+                    user: result[0],
+                });
+            }
+            return res.status(401).send({
+                msg: "Username or password is incorrect!",
+            });
+        });
+    });
+});
+
+router.post("/likeMessage");
 
 module.exports = router;
